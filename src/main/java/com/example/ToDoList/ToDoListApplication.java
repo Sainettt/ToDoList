3d6a59b94fb5ee99c50d6c93be57e160d12251f4
NikitaTolstykh <nikitatolstyh7@gmail.com>
2024-03-02 19:07:24 +0100

    3-st commit

--- src/main/java/com/example/ToDoList/ToDoListApplication.java
+++ src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -9,6 +9,7 @@ public class ToDoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 		System.out.println();
+		System.out.println("Nikita");
 	}
 
 }
