a1ff4822f58c0a5b3a09eebf24d27898a05369e0
Danyil Fiut <danyilfiut@gmail.com>
2024-03-02 19:32:20 +0100

    commit 2

--- src/main/java/com/example/ToDoList/ToDoListApplication.java
+++ src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -8,7 +8,11 @@ public class ToDoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
-		System.out.println();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i; j++) {
+				j++;
+			}
+		}
 	}
 
 }
