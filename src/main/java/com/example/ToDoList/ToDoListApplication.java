8a4761369a406f0ea6c1961ad625390f71f39123
Danyil Fiut <danyilfiut@gmail.com>
2024-03-02 20:25:19 +0100

    start serv

--- src/main/java/com/example/ToDoList/ToDoListApplication.java
+++ src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -2,12 +2,23 @@ package com.example.ToDoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
-	}
 
+	}
+	@Controller
+	public static class HomeController {
+
+		@GetMapping("/")
+		public String home() {
+			return "index";
+		}
+	}
 }
+
